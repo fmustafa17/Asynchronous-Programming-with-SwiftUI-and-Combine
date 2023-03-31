6af20817a28a2cc2bb217e20e7de6b976d8075ea
Farhana <farhana.swe@gmail.com>
2023-03-31 07:12:40 -0700

    add code

--- Chapter 08 - Driving UI State with Combine/starter/SignUpForm/SignUpForm/SignUpForm.swift
+++ Chapter 08 - Driving UI State with Combine/starter/SignUpForm/SignUpForm/SignUpForm.swift
@@ -10,60 +10,114 @@ import Combine
 
 // MARK: - View Model
 class SignUpFormViewModel: ObservableObject {
-  
-  // MARK: Input
-  @Published var username: String = ""
-  @Published var password: String = ""
-  @Published var passwordConfirmation: String = ""
-  
-  // MARK: Output
-  @Published var usernameMessage: String = ""
-  @Published var passwordMessage: String = ""
-  @Published var isValid: Bool = false
+    
+    // MARK: Input
+    @Published var username: String = ""
+    @Published var password: String = ""
+    @Published var passwordConfirmation: String = ""
+    
+    // MARK: Output
+    @Published var usernameValidationMessage: String = ""
+    @Published var passwordMessage: String = ""
+    @Published var isValid: Bool = false
+    
+    private lazy var isUsernameLengthValidPublisher: AnyPublisher<Bool, Never> = {
+        $username
+            .map { username in
+                username.count >= 3 // implicit return
+            }
+            .eraseToAnyPublisher()
+    }()
+    
+    private lazy var isPasswordEmptyPublisher: AnyPublisher<Bool, Never> = {
+        $password
+            .map(\.isEmpty)
+            .eraseToAnyPublisher()
+    }()
+    
+    private lazy var isPasswordMatching: AnyPublisher<Bool, Never> = {
+        Publishers.CombineLatest($password, $passwordConfirmation)
+            .map(==)
+            .eraseToAnyPublisher()
+    }()
+    
+    private lazy var isPasswordValidPublisher: AnyPublisher<Bool, Never> = {
+        Publishers.CombineLatest(isPasswordEmptyPublisher, isPasswordMatching)
+            .map { !$0 && $1 }
+            .eraseToAnyPublisher()
+    }()
+    
+    private lazy var isFormValidPublisher: AnyPublisher<Bool, Never> = {
+        Publishers.CombineLatest(isUsernameLengthValidPublisher, isPasswordValidPublisher)
+            .map { $0 && $1 }
+            .eraseToAnyPublisher()
+    }()
+    
+    init() {
+        isFormValidPublisher
+            .assign(to: &$isValid)
+        
+        isUsernameLengthValidPublisher
+            .map {
+                $0 ? "" : "Username needs to be at least 3 characters"
+            }
+            .assign(to: &$usernameValidationMessage)
+        
+        Publishers.CombineLatest(isPasswordEmptyPublisher, isPasswordMatching)
+            .map { passwordIsEmpty, passwordsMatch in
+                if passwordIsEmpty {
+                    return "Password must not be empty"
+                } else if !passwordsMatch {
+                    return "Passwords do not match"
+                }
+                return ""
+            }
+            .assign(to: &$passwordMessage)
+    }
 }
 
 // MARK: - View
 struct SignUpForm: View {
-  @StateObject var viewModel = SignUpFormViewModel()
-  
-  var body: some View {
-    Form {
-      // Username
-      Section {
-        TextField("Username", text: $viewModel.username)
-          .autocapitalization(.none)
-          .disableAutocorrection(true)
-      } footer: {
-        Text(viewModel.usernameMessage)
-          .foregroundColor(.red)
-      }
-      
-      // Password
-      Section {
-        SecureField("Password", text: $viewModel.password)
-        SecureField("Repeat password", text: $viewModel.passwordConfirmation)
-      } footer: {
-        Text(viewModel.passwordMessage)
-          .foregroundColor(.red)
-      }
-      
-      // Submit button
-      Section {
-        Button("Sign up") {
-          print("Signing up as \(viewModel.username)")
+    @StateObject var viewModel = SignUpFormViewModel()
+    
+    var body: some View {
+        Form {
+            // Username
+            Section {
+                TextField("Username", text: $viewModel.username)
+                    .autocapitalization(.none)
+                    .disableAutocorrection(true)
+            } footer: {
+                Text(viewModel.usernameValidationMessage)
+                    .foregroundColor(.red)
+            }
+            
+            // Password
+            Section {
+                SecureField("Password", text: $viewModel.password)
+                SecureField("Repeat password", text: $viewModel.passwordConfirmation)
+            } footer: {
+                Text(viewModel.passwordMessage)
+                    .foregroundColor(.red)
+            }
+            
+            // Submit button
+            Section {
+                Button("Sign up") {
+                    print("Signing up as \(viewModel.username)")
+                }
+                .disabled(!viewModel.isValid)
+            }
         }
-        .disabled(!viewModel.isValid)
-      }
     }
-  }
 }
 
 // MARK: - Preview
 struct SignUpForm_Previews: PreviewProvider {
-  static var previews: some View {
-    NavigationStack {
-      SignUpForm()
-        .navigationTitle("Sign up")
+    static var previews: some View {
+        NavigationStack {
+            SignUpForm()
+                .navigationTitle("Sign up")
+        }
     }
-  }
 }
